3c8bcc5120d5a0fe777c4a231870e1914601c788
Luis Pedro Galarza Torres <lpgalarzat@gmail.com>
2023-09-07 16:17:57 -0500

    Agregar archivos de proyecto.

---
+++ APIPruebaLGDTO/CrudDTO.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIPruebaLGDTO
+{
+    public class departamentoDTO
+    {
+        public short codigoDepartamento { get; set; }
+        public string nombreDepartamento { get; set; }
+    }
+
+    public class cargosDTO
+    {
+        public short codigoCargo { get; set; }
+        public string descripcionCargo { get; set; }
+    }
+
+    public class usuariosDTO
+    {
+        public string? codigoUsuario { get; set; }
+        public short codigoDepartamento { get; set; }
+        public string? nombreDepartamento { get; set; }
+        public short codigoCargo { get; set; }
+        public string? descripcionCargo { get; set; }
+        public string? nombres { get; set; }
+        public string? apellidos { get; set; }
+        public string? email { get; set; }
+        public bool? estadoUsuario { get; set; }
+    }
+
+}
